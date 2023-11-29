bd7add35a8b804dede5000ea7e9884dffa8e3b9a
Julien Bernard <julienbernardjb88@gmail.com>
2023-11-29 11:25:51 +0100

    Html et app de base pour prononciation de mots avec des cartes sur l'html

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// Fonction pour prononcer un mot
+function prononcerMot(mot) {
+    // Vérifiez si la synthèse vocale est prise en charge par le navigateur
+    if ('speechSynthesis' in window) {
+        // Créez une nouvelle instance de l'objet SpeechSynthesisUtterance
+        var utterance = new SpeechSynthesisUtterance(mot);
+
+        // Utilisez l'API Web Speech pour prononcer le mot
+        speechSynthesis.speak(utterance);
+    } else {
+        alert("La synthèse vocale n'est pas prise en charge par votre navigateur.");
+    }
+}

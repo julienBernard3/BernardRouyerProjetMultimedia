5e3498309051d41c5c384caa4300b81f29da3771
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2023-12-01 11:44:35 +0100

    Ajout de la sélection de la langue (la langue ne ce met pas à jour automatiquement)

--- src/utilitaire.js
+++ src/utilitaire.js
@@ -1,6 +1,20 @@
 // utilitaire.js
 
-var mots = ['Chat', 'Chien', 'Oiseau', 'Cheval'];
+const nomsAnimaux = {
+    francais: ['Chat', 'Cheval', 'Chien', 'Chèvre', 'Mouton', 'Oiseau', 'Poisson', 'Poule', 'Vache'],
+    anglais: ['Cat', 'Horse', 'Dog', 'Goat', 'Sheep', 'Bird', 'Fish', 'Chicken', 'Cow'],
+    allemand: ['Katze', 'Pferd', 'Hund', 'Ziege', 'Schaf', 'Vogel', 'Fisch', 'Huhn', 'Kuh'],
+    espagnol: ['Gato', 'Caballo', 'Perro', 'Cabra', 'Oveja', 'Pájaro', 'Pescado', 'Pollo', 'Vaca'],
+    italien: ['Gatto', 'Cavallo', 'Cane', 'Capra', 'Pecora', 'Uccello', 'Pesce', 'Pollo', 'Mucca']
+};
+var mots = nomsAnimaux['francais'];
+
+function mettreAJourNoms() {
+    const langueSelectionnee = document.getElementById('langue').value;
+
+    // Renvoyer la liste des noms correspondant à la langue sélectionnée
+    mots = nomsAnimaux[langueSelectionnee];
+}
 
 // Fonction pour afficher la page d'apprentissage
 function afficherPage(page) {
@@ -32,7 +46,7 @@ function afficherCartes(mots, container, clickHandler) {
         card.className = 'card';
         card.onclick = clickHandler;
         var image = document.createElement('img');
-        image.src = "images/" + mots[i].toLowerCase() + ".jpg";
+        image.src = "images/" + nomsAnimaux["francais"][i] + ".jpg";
         image.style.maxWidth = "100%";
         image.alt = mots[i];
         card.appendChild(image);
